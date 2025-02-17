9c578688301e0a8a6f3f859521b1896a4aa1a3ad
Gonzalo Ramos <gonzaloalexramos@gmail.com>
2025-02-17 18:40:49 -0300

    analiticas y velocidad

--- app/layout.js
+++ app/layout.js
@@ -4,33 +4,37 @@ import NavbarConsumidores from "@/components/page-components/consumidores/Navbar
 import Footer from "@/components/page-components/principales/Footer";
 import WhatsappBtn from "@/components/personalized-ui/WhatsappBtn";
 import {ToastContainer} from "react-toastify";
+import {Analytics} from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+    src: "./fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
+    weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+    src: "./fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "100 900",
 });
 
 export const metadata = {
-  title: "Garret Games",
-  description: "Listado de ofertas",
+    title: "Garret Games",
+    description: "Listado de ofertas",
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="es">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
-      >
+export default function RootLayout({children}) {
+    return (
+        <html lang="es">
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
+        >
         {children}
+        <Analytics/>
+        <SpeedInsights/>
         <ToastContainer/>
-        <WhatsappBtn />
-      </body>
-    </html>
-  );
+        <WhatsappBtn/>
+        </body>
+        </html>
+    );
 }
