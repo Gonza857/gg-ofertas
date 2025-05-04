e22b5d592355ae5ca2cd3b2efcfe2e6196813d4d
Gonzalo Ramos <gonzaloalexramos@gmail.com>
2025-05-03 21:24:18 -0300

    agrego para ver el estado

--- dominio/utils/auth/cookiesManager.js
+++ dominio/utils/auth/cookiesManager.js
@@ -23,8 +23,11 @@ class CookieManager {
 
     static get (cookieStore, cookieName) {
         const token = cookieStore.get(cookieName)?.value;
+        console.log("token obtenido", token)
         if (token === "undefined" || token === undefined) return null;
+        console.log("el token existe")
         let {payload: data} = jwt.verify(token, process.env.SECRET_JWT_KEY);
+        console.log("data del token", data)
         return data;
     }
 }
